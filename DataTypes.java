9e0de31583ef08314706febc7ab017d1ae3c9905
Thu Vo <31901141+ThuVo22@users.noreply.github.com>
2017-10-10 14:56:25 -0500

    Update DataTypes.java

--- DataTypes.java
+++ DataTypes.java
@@ -1,3 +1,39 @@
+
+
+import java.util.*;
+import java.io.*;
+
+
+
+class DataTypes{
+    public static void main(String []argh)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++)
+        {
+            try
+            {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)System.out.println("* byte");
+                //Complete the code
+                if (x>=-32768 && x<=32767)System.out.println("* short");
+                if (x>=-Math.pow(2,31) && x<=Math.pow(2,31)-1)System.out.println("* int");
+                if (x>=-Math.pow(2,63) && x<=Math.pow(2,63)-1)System.out.println("* long");
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+
+        }
+    }
+}
+
+
+//////////////////////////////////////////////////////////////
 Java has 8 primitive data types; char, boolean, byte, short, int, long, float, and double. For this exercise, we'll work with the primitives used to hold integer values (byte, short, int, and long):
 
 A byte is an 8-bit signed integer.
@@ -56,35 +92,3 @@ Explanation
 2333333333333333 is very large and is outside of the allowable range of values for the primitive data types discussed in this problem.
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-import java.util.*;
-import java.io.*;
-
-
-
-class DataTypes{
-    public static void main(String []argh)
-    {
-        Scanner sc = new Scanner(System.in);
-        int t=sc.nextInt();
-
-        for(int i=0;i<t;i++)
-        {
-            try
-            {
-                long x=sc.nextLong();
-                System.out.println(x+" can be fitted in:");
-                if(x>=-128 && x<=127)System.out.println("* byte");
-                //Complete the code
-                if (x>=-32768 && x<=32767)System.out.println("* short");
-                if (x>=-Math.pow(2,31) && x<=Math.pow(2,31)-1)System.out.println("* int");
-                if (x>=-Math.pow(2,63) && x<=Math.pow(2,63)-1)System.out.println("* long");
-            }
-            catch(Exception e)
-            {
-                System.out.println(sc.next()+" can't be fitted anywhere.");
-            }
-
-        }
-    }
-}
